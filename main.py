b575ee1e52a15d2f0fb67d5166c158c5ca73f698
kevin-f-liu <kevin.f.liu2012@gmail.com>
2017-07-24 23:14:08 -0400

    added visualizer

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import numpy as np
 import math
 import Elements as el
 import FileRead as fr
+from tkinter import *
 
 
-file = fr.FileRead("iteration2.txt")
+file = fr.FileRead("iteration4.txt")
 b = file.getBridge()
 
 bridge = next(b)
@@ -98,6 +99,11 @@ for node in nodes:
     memberForcesEq.append(fx)
     memberForcesEq.append(fy)
 
+print([len(n) for n in memberForces])
+print(len(nodes))
+print(len(memberForcesEq))
+#print(np.linalg.det(memberForces))
+
 result = [0 if math.fabs(num) < 1e-14 else num for num in (np.linalg.solve(memberForces, memberForcesEq)).tolist()]
 
 for i in range(0, len(trusses), 2):
@@ -107,9 +113,31 @@ for i in range(0, len(trusses), 2):
 print(result)
 print(cost)
 
+
+
+def createGui():
+    radius = 3
+    width = 1000
+    height = 750
+    centerX = width/2 - 150
+    centerY = height / 2
+    scale = 30
+    root = Tk()
+    w = Canvas(root, width=width, height=height)
+    w.pack()
+
+    for node in nodes:
+        w.create_oval(node.x*scale + centerX - radius, -node.y*scale + centerY - radius, node.x*scale + centerX + radius, -node.y*scale + centerY + radius)
+    for i in range(0, len(trusses), 2):
+        w.create_line(trusses[i].startx*scale + centerX, -trusses[i].starty*scale + centerY, trusses[i].endx*scale + centerX, -trusses[i].endy*scale + centerY)
+
+    root.mainloop()
+
 def printLinks():
     for node in nodes:
         print(node.id, end=": ")
         for t in node.connections:
             print(t.startID, "%s(%s)" % (t.endID, t.length), sep='-', end=' ')
         print()
+
+createGui()
